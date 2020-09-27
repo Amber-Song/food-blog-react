a1735fa3fb578d67a07bdeabdd78fb6b55843244
Amber Song <jihongsongnz@gmail.com>
2020-09-28 12:17:51 +1300

    Change class component to functional component

--- src/components/NavBarCity.js
+++ src/components/NavBarCity.js
@@ -1,46 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-class NavBarCity extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showCityList: false
-    };
-    this.displayCityList = this.displayCityList.bind(this);
-  }
+function NavBarCity() {
+  const [showCityList, setShowCityList] = useState(false);
 
-  displayCityList() {
-    this.setState(preState => {
-      return { showCityList: !preState.showCityList };
-    });
-  }
+  const displayCityList = () => {
+    setShowCityList(!showCityList);
+  };
 
-  render() {
-    return (
-      <div>
-        {this.state.showCityList ? (
-          <div className="bar-content" onClick={this.displayCityList}>
-            <div>City: </div>
-            <hr />
-            <Link to="/FoodBlog/Auckland"> Auckland </Link> <br />
-            <Link to="/FoodBlog/Dargaville"> Dargaville </Link> <br />
-            <Link to="/FoodBlog/Dunedin"> Dunedin </Link> <br />
-            <Link to="/FoodBlog/Hamilton"> Hamilton </Link> <br />
-            <Link to="/FoodBlog/Invercargill"> Invercargill </Link> <br />
-            <Link to="/FoodBlog/Kerikeri"> Kerikeri </Link> <br />
-            <Link to="/FoodBlog/Paihia"> Paihia </Link> <br />
-            <Link to="/FoodBlog/Wanaka"> Wanaka </Link> <br />
-            <Link to="/FoodBlog/Whangarei"> Whangarei </Link> <br />
-          </div>
-        ) : (
-          <div className="bar-title" onClick={this.displayCityList}>
-            City
-          </div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {showCityList ? (
+        <div className="bar-content" onClick={displayCityList}>
+          <div>City: </div>
+          <hr />
+          <Link to="/FoodBlog/Auckland"> Auckland </Link> <br />
+          <Link to="/FoodBlog/Dargaville"> Dargaville </Link> <br />
+          <Link to="/FoodBlog/Dunedin"> Dunedin </Link> <br />
+          <Link to="/FoodBlog/Hamilton"> Hamilton </Link> <br />
+          <Link to="/FoodBlog/Invercargill"> Invercargill </Link> <br />
+          <Link to="/FoodBlog/Kerikeri"> Kerikeri </Link> <br />
+          <Link to="/FoodBlog/Paihia"> Paihia </Link> <br />
+          <Link to="/FoodBlog/Wanaka"> Wanaka </Link> <br />
+          <Link to="/FoodBlog/Whangarei"> Whangarei </Link> <br />
+        </div>
+      ) : (
+        <div className="bar-title" onClick={displayCityList}>
+          City
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default NavBarCity;
